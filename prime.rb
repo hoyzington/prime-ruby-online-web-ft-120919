498cdee2d9f8f85b3d39062f7cade34839d1962e
Scott Hoisington <hoyzington@gmail.com>
2019-12-12 13:26:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(n)
   return false if n < 2
   return true if n == 2
   range_limit = n-1
-  range = 
-  return false if (2..range_limit).to_a.include? {|x| n % x == 0}
+  range = (2..range_limit).to_a
+  return false if range.include? {|x| n % x == 0}
 end  
